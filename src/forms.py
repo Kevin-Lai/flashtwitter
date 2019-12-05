f8d0b1c4445df251756689d13e83d27ed833a5e2
Kevin-Lai <kevinlai249@gmail.com>
2019-12-04 17:47:03 -0800

    Updated forms to accept autocomplete data

--- src/forms.py
+++ src/forms.py
@@ -5,12 +5,15 @@ Author: Kevin Lai
 '''
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, StringField, IntegerField, SubmitField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import DataRequired, Length
+#from wtforms import InputField
+# InputField validator does not work with AutoComplete data
+# Use InputField validator for secondary confirmation, like to confirm email
 
 class CreateTweetForm(FlaskForm):
-	description = TextAreaField('description', validators=[InputRequired(), Length(min = 1, max = 280)])
+	description = TextAreaField('description', validators=[DataRequired(), Length(min = 1, max = 280)])
 	create_confirm = SubmitField('Create Tweet')
 
 class DeleteTweetForm(FlaskForm):
-	tweet_id = IntegerField('tweet_id', validators=[InputRequired()])
+	tweet_id = IntegerField('tweet_id', validators=[DataRequired()])
 	delete_confirm = SubmitField('Delete Tweet')
